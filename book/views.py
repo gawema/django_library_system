a3b2c1816d398daaddf22ad4972ae5ca6e7a4fc7
gawema <gab.mannu@hotmail.it>
2020-03-29 19:00:27 +0200

    add authentication

--- book/views.py
+++ book/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -9,12 +11,14 @@ from .models import Book, Loan
 
 logger = logging.getLogger(__name__)
 
+@login_required
 def index(request):
 	books = Book.objects.all()
 	available_books = Book.objects.filter(booked=False)
 	non_available_books = Book.objects.filter(booked=True)
 	loans =  Loan.objects.all()
 	context = {
+		'user': request.user,
 		'books': books,
 		'aBooks': available_books,
 		'bBooks': non_available_books,
@@ -22,6 +26,7 @@ def index(request):
 	}
 	return render(request, 'book/index.html', context)
 
+@login_required
 def loanBook(request):
 	if request.method == "POST":
 		id = request.POST["id"]
@@ -30,6 +35,7 @@ def loanBook(request):
 		book.save()
 
 		loan = Loan()
+		loan.customer = request.user
 		loan.book = book
 		loan.startDate = date.today()
 		loan.endDate = date.today()+ relativedelta(months=1)
@@ -38,6 +44,7 @@ def loanBook(request):
 
 	return HttpResponseRedirect(reverse('book:index'))
 
+@login_required
 def returnBook(request): 
 	if request.method == "POST":
 		id = request.POST["id"]
