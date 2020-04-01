404b2add7f7480410bfd3499da24471fc165870e
gawema <gab.mannu@hotmail.it>
2020-04-01 13:11:33 +0200

    Prettyfied and emptied db

--- book/views.py
+++ book/views.py
@@ -11,144 +11,147 @@ from .models import Book, Loan
 
 logger = logging.getLogger(__name__)
 
+
 @login_required
 def home(request):
-	if request.user.is_staff == True:
-		return HttpResponseRedirect(reverse('book:allLoans'))
-	available_books = Book.objects.filter(booked=False, magazine=False)
-	available_magazines = Book.objects.filter(booked=False, magazine=True)
-	reminders = []
-	loans =  Loan.objects.filter(customer = request.user)
-	for loan in loans:
-		if loan.reminder is not None:
-			reminders.append(loan.reminder)
+    if request.user.is_staff == True:
+        return HttpResponseRedirect(reverse('book:allLoans'))
+    available_books = Book.objects.filter(booked=False, magazine=False)
+    available_magazines = Book.objects.filter(booked=False, magazine=True)
+    reminders = []
+    loans = Loan.objects.filter(customer=request.user)
+    for loan in loans:
+        if loan.reminder is not None:
+            reminders.append(loan.reminder)
 
-	logger.error(reminders)
-	context = {
-		'user': request.user,
-		'books': available_books,
-		'magazines': available_magazines,
-		'reminders': reminders
-	}
-	return render(request, 'book/home.html', context)
+    context = {
+        'user': request.user,
+        'books': available_books,
+        'magazines': available_magazines,
+        'reminders': reminders
+    }
+    return render(request, 'book/home.html', context)
+
 
 @login_required
 def loans(request):
-	if request.user.is_staff == True:
-		return HttpResponseRedirect(reverse('book:allLoans'))
-	loans =  Loan.objects.filter(customer = request.user)
-	today = date.today()
-	context = {
-		'user': request.user,
-		'loans': loans,
-		'today': today
-	}
-	return render(request, 'book/loans.html', context)
+    if request.user.is_staff == True:
+        return HttpResponseRedirect(reverse('book:allLoans'))
+    loans = Loan.objects.filter(customer=request.user)
+    today = date.today()
+    context = {
+        'user': request.user,
+        'loans': loans,
+        'today': today
+    }
+    return render(request, 'book/loans.html', context)
 
 
 @login_required
 def requestBook(request):
-	logger.error('HERE')
-	loans = Loan.objects.filter(customer = request.user)
-	bookCount = 0
-	outstandings = 0
-	for loan in loans:
-		if loan.book.magazine == False:
-			bookCount += 1
-		if loan.endDate < date.today():
-			outstandings += 1
-	if bookCount >= 10 or outstandings >= 1:
-		logger.error("ciaooo")
-		return HttpResponseRedirect(reverse('book:home'))
-	return confirmLoan(request, 'book')
+    loans = Loan.objects.filter(customer=request.user)
+    bookCount = 0
+    outstandings = 0
+    for loan in loans:
+        if loan.book.magazine == False:
+            bookCount += 1
+        if loan.endDate < date.today():
+            outstandings += 1
+    if bookCount >= 10 or outstandings >= 1:
+        return HttpResponseRedirect(reverse('book:home'))
+    return confirmLoan(request, 'book')
 
 
 @login_required
 def requestMagazine(request):
-	loans = Loan.objects.filter(customer = request.user)
-	magazineCount = 0
-	outstandings = 0
+    loans = Loan.objects.filter(customer=request.user)
+    magazineCount = 0
+    outstandings = 0
 
-	for loan in loans:
-		if loan.book.magazine == True:
-			magazineCount += 1
-		if loan.endDate <= date.today():
-			outstandings += 1
-	if magazineCount >= 3 or outstandings >= 1:
-		return HttpResponseRedirect(reverse('book:home'))
-	return confirmLoan(request, 'magazine')
+    for loan in loans:
+        if loan.book.magazine == True:
+            magazineCount += 1
+        if loan.endDate <= date.today():
+            outstandings += 1
+    if magazineCount >= 3 or outstandings >= 1:
+        return HttpResponseRedirect(reverse('book:home'))
+    return confirmLoan(request, 'magazine')
 
 
 @login_required
 def confirmLoan(request, type):
-	if request.method == "POST":
-		id = request.POST["id"]
-		book = get_object_or_404(Book, pk=id)
-		book.booked = True
-		book.save()
+    if request.method == "POST":
+        id = request.POST["id"]
+        book = get_object_or_404(Book, pk=id)
+        book.booked = True
+        book.save()
 
-		loan = Loan()
-		loan.customer = request.user
-		loan.book = book
-		loan.startDate = date.today()
-		if (type == 'magazine'):
-			loan.endDate = date.today() + relativedelta(days=7)
-		elif (type == 'book'):
-			loan.endDate = date.today() + relativedelta(months=1)
-		
-		loan.save()
+        loan = Loan()
+        loan.customer = request.user
+        loan.book = book
+        loan.startDate = date.today()
+        if (type == 'magazine'):
+            loan.endDate = date.today() + relativedelta(days=7)
+        elif (type == 'book'):
+            loan.endDate = date.today() + relativedelta(months=1)
 
-	return HttpResponseRedirect(reverse('book:home'))
+        loan.save()
+
+    return HttpResponseRedirect(reverse('book:home'))
+
 
 @login_required
-def returnBook(request): 
-	if request.method == "POST":
-		id = request.POST["id"]
-		loan = get_object_or_404(Loan, pk=id)
-		bookId = loan.book.id;
-		book = get_object_or_404(Book, pk=bookId)
-		book.booked = False
-		book.save()
-		loan.delete()
+def returnBook(request):
+    if request.method == "POST":
+        id = request.POST["id"]
+        loan = get_object_or_404(Loan, pk=id)
+        bookId = loan.book.id
+        book = get_object_or_404(Book, pk=bookId)
+        book.booked = False
+        book.save()
+        loan.delete()
 
-	return HttpResponseRedirect(reverse('book:loans'))
+    return HttpResponseRedirect(reverse('book:loans'))
 
 
 @login_required
 def oustandingLoans(request):
-	if request.user.is_staff == False:
-		return HttpResponseRedirect(reverse('book:home'))
-	loans = Loan.objects.all()
-	oustanding_loans = []
-	for loan in loans:
-		if loan.endDate < date.today():
-			oustanding_loans.append(loan)
-	context = {
-		'user': request.user,
-		'loans': oustanding_loans,
-	}
-	return render(request, 'book/oustanding_loans.html', context)
+    if request.user.is_staff == False:
+        return HttpResponseRedirect(reverse('book:home'))
+    loans = Loan.objects.all()
+    oustanding_loans = []
+    for loan in loans:
+        if loan.endDate < date.today():
+            oustanding_loans.append(loan)
+    context = {
+        'user': request.user,
+        'loans': oustanding_loans,
+    }
+    return render(request, 'book/oustanding_loans.html', context)
+
 
 @login_required
 def allLoans(request):
-	if request.user.is_staff == False:
-		return HttpResponseRedirect(reverse('book:home'))
-	loans = Loan.objects.all()
-	request.user.is_staff
-	context = {
-		'user': request.user,
-		'loans': loans,
-	}
-	return render(request, 'book/all_loans.html', context)
+    if request.user.is_staff == False:
+        return HttpResponseRedirect(reverse('book:home'))
+    loans = Loan.objects.all()
+    request.user.is_staff
+    context = {
+        'user': request.user,
+        'loans': loans,
+    }
+    return render(request, 'book/all_loans.html', context)
+
 
 @login_required
 def sendReminder(request):
-	if request.user.is_staff == False:
-		return HttpResponseRedirect(reverse('book:home'))
-	if request.method == "POST":
-		id = request.POST["id"]
-		loan = get_object_or_404(Loan, pk=id)
-		loan.reminder = "Reminder: '"+loan.book.title+"' needs to be returned as soon as possible"
-		loan.save() 
+    if request.user.is_staff == False:
+        return HttpResponseRedirect(reverse('book:home'))
+    if request.method == "POST":
+        id = request.POST["id"]
+        loan = get_object_or_404(Loan, pk=id)
+        loan.reminder = "Reminder: '"+loan.book.title + \
+            "' needs to be returned as soon as possible"
+        loan.save()
 
-	return HttpResponseRedirect(reverse('book:oustandingLoans'))
+    return HttpResponseRedirect(reverse('book:oustandingLoans'))
--- login/views.py
+++ login/views.py
@@ -9,106 +9,108 @@ from django.http import HttpResponseRedirect
 from . models import PasswordResetRequest
 
 
-
 def login(request):
-   context = {}
+    context = {}
 
-   if request.method == "POST":
-      user = authenticate(request, username=request.POST['user'], password=request.POST['password'])
-      if user:
+    if request.method == "POST":
+        user = authenticate(
+            request, username=request.POST['user'], password=request.POST['password'])
+        if user:
             dj_login(request, user)
             return HttpResponseRedirect(reverse('book:home'))
-      else:
+        else:
             context = {
-               'error': 'Bad username or password.'
+                'error': 'Bad username or password.'
             }
-   return render(request, 'login/login.html', context)
+    return render(request, 'login/login.html', context)
 
 
 def logout(request):
-   dj_logout(request)
-   return render(request, 'login/login.html')
+    dj_logout(request)
+    return render(request, 'login/login.html')
+
 
 def request_password_reset(request):
-   if request.method == "POST":
-      post_user = request.POST['username']
-      user = None
+    if request.method == "POST":
+        post_user = request.POST['username']
+        user = None
 
-      if post_user:
-         try:
-            user = User.objects.get(username=post_user)
-         except:
-            print(f"Invalid password request: {post_user}")
-      else:
-         post_user = request.POST['email']
-         try:
-            user = User.objects.get(email=post_user)
-         except:
-            print(f"Invalid password request: {post_user}")
-      if user:
-         prr = PasswordResetRequest()
-         prr.user = user
-         prr.save()
-         print(prr)
-         return HttpResponseRedirect(reverse('login:password_reset'))
-         
-   return render(request, 'login/request_password_reset.html')
+        if post_user:
+            try:
+                user = User.objects.get(username=post_user)
+            except:
+                print(f"Invalid password request: {post_user}")
+        else:
+            post_user = request.POST['email']
+            try:
+                user = User.objects.get(email=post_user)
+            except:
+                print(f"Invalid password request: {post_user}")
+        if user:
+            prr = PasswordResetRequest()
+            prr.user = user
+            prr.save()
+            print(prr)
+            return HttpResponseRedirect(reverse('login:password_reset'))
+
+    return render(request, 'login/request_password_reset.html')
 
 
 def password_reset(request):
-   if request.method == "POST":
-      post_user = request.POST['username']
-      password = request.POST['password']
-      confirm_password = request.POST['confirm_password']
-      token = request.POST['token']
+    if request.method == "POST":
+        post_user = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        token = request.POST['token']
 
-      if password == confirm_password:
-         try:
-            prr = PasswordResetRequest.objects.get(token=token)
-            prr.save()
-         except:
-            print("Invalid password reset attempt.")
-            return render(request, 'login/password_reset.html')
+        if password == confirm_password:
+            try:
+                prr = PasswordResetRequest.objects.get(token=token)
+                prr.save()
+            except:
+                print("Invalid password reset attempt.")
+                return render(request, 'login/password_reset.html')
 
-         user = prr.user
-         user.set_password(password)
-         user.save()
-         return HttpResponseRedirect(reverse('login:login'))
+            user = prr.user
+            user.set_password(password)
+            user.save()
+            return HttpResponseRedirect(reverse('login:login'))
 
-   return render(request, 'login/password_reset.html')
+    return render(request, 'login/password_reset.html')
 
 
 def sign_up(request):
-   context = {}
-   if request.method == "POST":
-      password = request.POST['password']
-      confirm_password = request.POST['confirm_password']
-      user_name = request.POST['user']
-      email = request.POST['email']
-      if password == confirm_password:
+    context = {}
+    if request.method == "POST":
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        user_name = request.POST['user']
+        email = request.POST['email']
+        if password == confirm_password:
             if User.objects.create_user(user_name, email, password):
-               return HttpResponseRedirect(reverse('login:login'))
+                return HttpResponseRedirect(reverse('login:login'))
             else:
-               context = {
-                  'error': 'Could not create user account - please try again.'
-               }
-      else:
+                context = {
+                    'error': 'Could not create user account - please try again.'
+                }
+        else:
             context = {
-               'error': 'Passwords did not match. Please try again.'
+                'error': 'Passwords did not match. Please try again.'
             }
-   return render(request, 'login/sign_up.html', context)
+    return render(request, 'login/sign_up.html', context)
+
 
 @login_required
 def delete_account(request):
-   if request.method == "POST":
-      if request.POST['confirm_deletion'] == "DELETE":
-            user = authenticate(request, username=request.user.username, password=request.POST['password'])
+    if request.method == "POST":
+        if request.POST['confirm_deletion'] == "DELETE":
+            user = authenticate(
+                request, username=request.user.username, password=request.POST['password'])
             if user:
-               print(f"Deleting user {user}")
-               user.delete()
-               return HttpResponseRedirect(reverse('login:login'))
+                print(f"Deleting user {user}")
+                user.delete()
+                return HttpResponseRedirect(reverse('login:login'))
             else:
-               print("fail delete")
+                print("fail delete")
 
-   return render(request, 'login/delete_account.html')
-
+    return render(request, 'login/delete_account.html')
